9656a8858525139dd75472bfb147028448765fc5
Jason <2045330878@qq.com>
2024-01-27 15:46:05 +0800

    [opt] upgrade solidity version

--- src/lst/BETH.sol
+++ src/lst/BETH.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "./interfaces/IBETH.sol";
+import "./interfaces/IERC20Errors.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import {IERC20Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 
 /**
  * @title Bang ETH Liquid Staked Token
--- src/stake/interfaces/IETHStakeManager.sol
+++ src/stake/interfaces/IETHStakeManager.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 interface IETHStakeManager {
     function stake(uint256 deadLine) external payable ;
