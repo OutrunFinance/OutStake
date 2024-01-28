5cd83df57e5a2ff6cfc6617aac4f43eaaf9d04eb
Jason <2045330878@qq.com>
2024-01-28 21:24:41 +0800

    [add] eth

--- src/lst/BETH.sol
+++ src/lst/BETH.sol
@@ -2,22 +2,14 @@
 pragma solidity ^0.8.20;
 
 import "./interfaces/IBETH.sol";
-import "./interfaces/IERC20Errors.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
  * @title Bang ETH Liquid Staked Token
  */
-contract BETH is IBETH, IERC20, IERC20Errors, AccessControl {
+contract BETH is IBETH, ERC20, AccessControl {
     address private _ETHStakeManager;
-
-    mapping(address account => uint256) private _balances;
-    mapping(address account => mapping(address spender => uint256)) private _allowances;
-
-    uint256 private _totalSupply;
-    string private _name = "Bang ETH";
-    string private _symbol = "BETH";
 
     modifier onlyETHStakeManager() {
         require(
@@ -27,36 +19,16 @@ contract BETH is IBETH, IERC20, IERC20Errors, AccessControl {
         _;
     }
 
-    constructor(address ETHStakeManager_) {
-        require(ETHStakeManager_ != address(0), "Zero address provided");
-        _ETHStakeManager = ETHStakeManager_;
-    }
-
-    function name() public view virtual returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view virtual returns (string memory) {
-        return _symbol;
-    }
-
-    function decimals() public view virtual returns (uint8) {
-        return 18;
-    }
-
-    function totalSupply() public view virtual returns (uint256) {
-        return _totalSupply;
+    constructor(address admin) ERC20("Bang ETH", "BETH") {
+        require(admin != address(0), "Zero address provided");
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
     }
 
     function ETHStakeManager() public view virtual returns (address) {
         return _ETHStakeManager;
     }
 
-    function balanceOf(address account) public view virtual returns (uint256) {
-        return _balances[account];
-    }
-
-    function setETHStakeManager(address _address) external override {
+    function setETHStakeManager(address _address) external override onlyRole(DEFAULT_ADMIN_ROLE) {
         require(_address != address(0), "Zero address provided");
 
         _ETHStakeManager = _address;
@@ -79,107 +51,5 @@ contract BETH is IBETH, IERC20, IERC20Errors, AccessControl {
      */
     function burn(address _account, uint256 _amount) external override onlyETHStakeManager {
         _burn(_account, _amount);
-    }
-
-    function transfer(address to, uint256 value) public virtual returns (bool) {
-        address owner = msg.sender;
-        _transfer(owner, to, value);
-        return true;
-    }
-
-    function allowance(address owner, address spender) public view virtual returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 value) public virtual returns (bool) {
-        address owner = msg.sender;
-        _approve(owner, spender, value);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 value) public virtual returns (bool) {
-        address spender = msg.sender;
-        _spendAllowance(from, spender, value);
-        _transfer(from, to, value);
-        return true;
-    }
-
-    function _transfer(address from, address to, uint256 value) internal {
-        if (from == address(0)) {
-            revert ERC20InvalidSender(address(0));
-        }
-        if (to == address(0)) {
-            revert ERC20InvalidReceiver(address(0));
-        }
-        _update(from, to, value);
-    }
-
-    function _update(address from, address to, uint256 value) internal virtual {
-        if (from == address(0)) {
-            _totalSupply += value;
-        } else {
-            uint256 fromBalance = _balances[from];
-            if (fromBalance < value) {
-                revert ERC20InsufficientBalance(from, fromBalance, value);
-            }
-            unchecked {
-                _balances[from] = fromBalance - value;
-            }
-        }
-
-        if (to == address(0)) {
-            unchecked {
-                _totalSupply -= value;
-            }
-        } else {
-            unchecked {
-                _balances[to] += value;
-            }
-        }
-
-        emit Transfer(from, to, value);
-    }
-
-    function _mint(address account, uint256 value) internal {
-        if (account == address(0)) {
-            revert ERC20InvalidReceiver(address(0));
-        }
-        _update(address(0), account, value);
-    }
-
-    function _burn(address account, uint256 value) internal {
-        if (account == address(0)) {
-            revert ERC20InvalidSender(address(0));
-        }
-        _update(account, address(0), value);
-    }
-
-    function _approve(address owner, address spender, uint256 value) internal {
-        _approve(owner, spender, value, true);
-    }
-
-    function _approve(address owner, address spender, uint256 value, bool emitEvent) internal virtual {
-        if (owner == address(0)) {
-            revert ERC20InvalidApprover(address(0));
-        }
-        if (spender == address(0)) {
-            revert ERC20InvalidSpender(address(0));
-        }
-        _allowances[owner][spender] = value;
-        if (emitEvent) {
-            emit Approval(owner, spender, value);
-        }
-    }
-
-    function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
-        uint256 currentAllowance = allowance(owner, spender);
-        if (currentAllowance != type(uint256).max) {
-            if (currentAllowance < value) {
-                revert ERC20InsufficientAllowance(spender, currentAllowance, value);
-            }
-            unchecked {
-                _approve(owner, spender, currentAllowance - value, false);
-            }
-        }
     }
 }
