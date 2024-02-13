674da57686c618bd350cc1c9d3e271157f9e30e7
Jason <2045330878@qq.com>
2024-02-13 20:12:09 +0800

    [opt] 放弃NFT方案，采用ERC20方案

--- src/stake/ETHStakeManager.sol
+++ src/stake/ETHStakeManager.sol
@@ -25,69 +25,68 @@ contract ETHStakeManager is IETHStakeManager, AccessControl {
     address public bETH;
     address public pETH;
     address public manager;
-    address public yieldPosition;
     uint256 public minIntervalTime;
-
-    mapping(uint256 account => uint256 positionId) public positions;    // positionId = NFT tokenId
+    uint256 public maxIntervalTime;
 
     /**
      * @param _bETH - Address of BETH Token
      * @param _pETH - Address of PETH Token
      * @param _admin - Address of the admin
      * @param _bot - Address of the Bot
-     * @param _yieldPosition - Address of the YieldPosition
      * @param _minIntervalTime - Min lock interval time
+     * @param _maxIntervalTime - Max lock interval time
      */
     constructor(
         address _bETH,
         address _pETH,
         address _admin,
         address _bot,
-        address _yieldPosition,
-        uint256 _minIntervalTime
+        uint256 _minIntervalTime,
+        uint256 _maxIntervalTime,
     ) {
         require(
             ((_bETH != address(0)) &&
             (_pETH != address(0)) &&
             (_admin != address(0)) &&
-            (_bot != address(0)) &&
-            (_yieldPosition != address(0))),
+            (_bot != address(0))),
             "Zero address provided"
         );
 
         bETH = _bETH;
         pETH = _pETH;
         minIntervalTime = _minIntervalTime;
-        yieldPosition =_yieldPosition;
+        maxIntervalTime =_maxIntervalTime;
 
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
     }
 
     /**
-     * 用户stake BETH，指定一个锁定到期时间deadLine，锁定到期前不可unstake，铸造相同数量的PETH和收益NFT
+     * 用户stake BETH，指定一个锁定到期时间deadLine，锁定到期前不可unstake，铸造相同数量的PETH和与锁定时间相关的收益Token
      *
-     * @dev Allows user to deposit BETH, then mints PETH and yieldPosition NFT for the user.
+     * @dev Allows user to deposit BETH, then mints PETH and YieldToken for the user.
      * @param amount BETH staked amount, amount % 1e15 == 0
      * @param deadLine User can unstake after deadLine
      */
     function stake(uint256 amount, uint256 deadLine) external override {
         require(amount % PRECISION == 0 && amount != 0, "Invalid Amount");
+        uint256 intervalTime = deadLine - block.timestamp;
         require(
-            deadLine - block.timestamp >= minIntervalTime,
-            "LockTime too short"
+            deadLine>= minIntervalTime + block.timestamp &&
+            deadLine <= maxIntervalTime + block.timestamp ,
+            "LockTime Invalid"
         );
 
         IERC20(bETH).safeTransferFrom(msg.sender, address(this), amount);
         IPETH(pETH).mint(msg.sender, amount);
 
-        // TODO 铸造NFT证明
+        // TODO 铸造YieldToken
 
         emit StakeETH(msg.sender, amount, deadLine);
     }
 
     /**
-     * 用户unstake,首先判断锁定时间是否已过期，否则不能unstake。t
-     * 最后将PETH销毁，仅将BETH取出来，只有作为收益证明的NFT才能将质押收益取出来。
+     * 用户unstake,首先判断锁定时间是否已过期，否则不能unstake。
+     * 然后将PETH销毁，仅将BETH取出来
      *
      * @dev Allows user to unstake funds
      * @param amount - Amount of BETH for burn
