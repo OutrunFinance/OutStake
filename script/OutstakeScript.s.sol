4165da1ae06c259fc7e1deae116cce098bedf820
Jason <2045330878@qq.com>
2024-03-15 17:44:11 +0800

    [opt] update deploy data

--- script/OutstakeScript.s.sol
+++ script/OutstakeScript.s.sol
@@ -5,11 +5,21 @@ import "./BaseScript.s.sol";
 import "../src/token/ETH/RETH.sol";
 import "../src/token/ETH/PETH.sol";
 import "../src/token/ETH/REY.sol";
+import "../src/token/USDB/RUSD.sol";
+import "../src/token/USDB/PUSD.sol";
+import "../src/token/USDB/RUY.sol";
 import "../src/stake/RETHStakeManager.sol";
+import "../src/stake/RUSDStakeManager.sol";
 import "../src/vault/OutETHVault.sol";
+import "../src/vault/OutUSDBVault.sol";
 
 contract OutstakeScript is BaseScript {
     function run() public broadcaster {
+        deployETH();
+        deployUSDB();
+    }
+
+    function deployETH() internal {
         RETH reth = new RETH(0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8);
         address rethAddress = address(reth);
 
@@ -31,7 +41,7 @@ contract OutstakeScript is BaseScript {
         stakeManager.initialize(vaultAddress, 30, 7, 365);
         address stakeAddress = address(stakeManager);
 
-        vault.initialize(stakeAddress, 0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8, 100, 15, 5);
+        // vault.initialize(stakeAddress, 0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8, 100, 15, 5);
         reth.initialize(vaultAddress);
         peth.initialize(stakeAddress);
         rey.initialize(stakeAddress);
@@ -41,5 +51,39 @@ contract OutstakeScript is BaseScript {
         console.log("REY deployed on %s", reyAddress);
         console.log("OutETHVault deployed on %s", vaultAddress);
         console.log("RETHStakeManager deployed on %s", stakeAddress);
+    }
+
+    function deployUSDB() internal {
+        RUSD rusd = new RUSD(0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8);
+        address rusdAddress = address(rusd);
+
+        PUSD pusd = new PUSD(0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8);
+        address pusdAddress = address(pusd);
+
+        RUY ruy = new RUY(0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8);
+        address ruyAddress = address(ruy);
+
+        OutUSDBVault vault = new OutUSDBVault(0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8, rusdAddress);
+        address vaultAddress = address(vault);
+
+        RUSDStakeManager stakeManager = new RUSDStakeManager(
+            0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8,
+            rusdAddress,
+            pusdAddress,
+            ruyAddress
+        );
+        stakeManager.initialize(vaultAddress, 30, 7, 365);
+        address stakeAddress = address(stakeManager);
+
+        // vault.initialize(stakeAddress, 0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8, 100, 15, 5);
+        rusd.initialize(vaultAddress);
+        pusd.initialize(stakeAddress);
+        ruy.initialize(stakeAddress);
+
+        console.log("RUSD deployed on %s", rusdAddress);
+        console.log("PUSD deployed on %s", pusdAddress);
+        console.log("RUY deployed on %s", ruyAddress);
+        console.log("OutUSDBVault deployed on %s", vaultAddress);
+        console.log("RUSDStakeManager deployed on %s", stakeAddress);
     }
 }
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 source ../.env
-forge clean && forge build
-# forge create --rpc-url sepolia \
+# forge clean && forge build
+# forge create --rpc-url blast_sepolia \
 # --constructor-args 0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8 0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8 0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8 0x20ae1f29849E8392BD83c3bCBD6bD5301a6656F8 100 \
 # --private-key $PRIVATE_KEY \
 # src/OutETHVault.sol:OutETHVault
-forge script OutstakeScript.s.sol:OutstakeScript --rpc-url sepolia --broadcast --verify --ffi -vvvv
+forge script OutstakeScript.s.sol:OutstakeScript --rpc-url blast_sepolia --broadcast --verify --ffi -vvvv
