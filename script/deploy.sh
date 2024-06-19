f357b6ff75cc7cce0827e24bf662db4a58a7784f
Jason <2045330878@qq.com>
2024-06-19 18:31:04 +0800

    redeploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,4 +3,4 @@ source ../.env
 forge script OutstakeScript.s.sol:OutstakeScript --rpc-url blast_sepolia \
     --priority-gas-price 300 --with-gas-price 1200000 \
     --optimize --optimizer-runs 100000 \
-    --broadcast --verify --ffi --resume -vvvv
+    --broadcast --verify --ffi -vvvv
