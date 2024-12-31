65696faf3617dfaee16efeb1fc1b746b06124b39
Jason <2045330878@qq.com>
2025-01-01 01:23:37 +0800

    add OutrunL2StakedTokenSY

--- src/core/StandardizedYield/implementations/OutrunL2StakedTokenSY.sol
+++ src/core/StandardizedYield/implementations/OutrunL2StakedTokenSY.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
-import { SYBase, ArrayLib } from "../../SYBase.sol";
-import { IExchangeRateOracle } from "../../../../oracles/interfaces/IExchangeRateOracle.sol";
+import { SYBase, ArrayLib } from "../SYBase.sol";
+import { IExchangeRateOracle } from "../../../oracles/interfaces/IExchangeRateOracle.sol";
 
-contract OutrunL2LRTSY is SYBase {
+contract OutrunL2StakedTokenSY is SYBase {
     address public exchangeRateOracle;
     address internal immutable underlyingAssetOnEthAddr;
     uint8 internal immutable underlyingAssetOnEthDecimals;
