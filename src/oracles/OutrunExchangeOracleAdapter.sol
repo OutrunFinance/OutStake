eda834d9e19e41cb0e64aad2d0a2da9f0b281283
Jason <2045330878@qq.com>
2025-01-01 01:23:19 +0800

    update oracle interface

--- src/oracles/OutrunExchangeOracleAdapter.sol
+++ src/oracles/OutrunExchangeOracleAdapter.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.26;
 
 import { IExchangeRateOracle } from "./interfaces/IExchangeRateOracle.sol";
-import { IChainlinkPriceFeed } from "./interfaces/IChainlinkPriceFeed.sol";
+import { AggregatorInterface } from "./interfaces/AggregatorInterface.sol";
 
-contract OutrunChainlinkRateOracleAdapter is IExchangeRateOracle {
+contract OutrunExchangeOracleAdapter is IExchangeRateOracle {
     address public immutable oracle;
     uint8 public immutable decimals;
     uint8 public immutable rawDecimals;
@@ -12,11 +12,11 @@ contract OutrunChainlinkRateOracleAdapter is IExchangeRateOracle {
     constructor(address _oracle, uint8 _decimals) {
         oracle = _oracle;
         decimals = _decimals;
-        rawDecimals = IChainlinkPriceFeed(_oracle).decimals();
+        rawDecimals = AggregatorInterface(_oracle).decimals();
     }
 
     function getExchangeRate() external view returns (uint256) {
-        int256 answer = IChainlinkPriceFeed(oracle).latestAnswer();
+        int256 answer = AggregatorInterface(oracle).latestAnswer();
         require(answer > 0, "Error answer");
         return (uint256(answer) * 10 ** decimals) / 10 ** rawDecimals;
     }
---
+++ src/oracles/interfaces/AggregatorInterface.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.26;
+
+interface AggregatorInterface {
+    function decimals() external view returns (uint8);
+
+    function description() external view returns (string memory);
+
+    function version() external view returns (uint256);
+
+    function latestAnswer() external view returns (int256);
+
+    function latestTimestamp() external view returns (uint256);
+
+    function latestRound() external view returns (uint256);
+
+    function getAnswer(uint256 roundId) external view returns (int256);
+
+    function getTimestamp(uint256 roundId) external view returns (uint256);
+
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
